c390c6d1270eb776529756a7cd9bb5c02c55bf4c
AntProg86 <anton456@rambler.ru>
2023-10-19 08:51:11 +0700

    test

---
+++ NumbersPuzzle/Models/testFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersPuzzle.Models
+{
+    public class testFile
+    {
+        public string test { get; set; }
+    }
+}
